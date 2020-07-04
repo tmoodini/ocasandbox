ad1d2625c011d6ad7d67bf123d1e374853229209
tmoodini <tom.moody@gmail.com>
2020-07-04 09:24:51 -0600

    Modified ShoppingCart

--- src/ocasandbox/ShoppingCart.java
+++ src/ocasandbox/ShoppingCart.java
@@ -1,10 +1,17 @@
 package ocasandbox;
 
+import java.math.BigInteger;
+
 public class ShoppingCart {
 
 	public static void main(String[] args) {
 		//tt
 
+		int a = 5, b = 1000;
+		int x;
+	
+		System.out.println(b + " ");
+		
 	}
 
 }
