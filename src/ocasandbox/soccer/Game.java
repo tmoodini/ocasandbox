4c083d305757c9678732f95e596a96010019133b
tmoodini <tom.moody@gmail.com>
2020-07-04 18:39:53 -0600

    Updated Soccer

--- src/ocasandbox/soccer/Game.java
+++ src/ocasandbox/soccer/Game.java
@@ -1,6 +1,7 @@
 package ocasandbox.soccer;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Game {
 	
@@ -28,6 +29,38 @@ public class Game {
 		}
 		
 		return this.goals.add(goal);
+	}
+	
+	public Goal randomGoal(double time) {
+		
+		Team scoringTeam;
+		Player scoringPlayer;
+		int scoringTeamIndex = new Random().nextInt(2);
+		if(scoringTeamIndex == 0) {
+			scoringTeam = this.homeTeam;
+			int randomPlayerIndex = new Random().nextInt(homeTeam.getRoster().length);
+			scoringPlayer = homeTeam.getRoster()[randomPlayerIndex];
+			homeGoals++;
+		}
+		else {
+			scoringTeam = this.awayTeam;
+			int randomPlayerIndex = new Random().nextInt(awayTeam.getRoster().length);
+			scoringPlayer = awayTeam.getRoster()[randomPlayerIndex];
+			
+			awayGoals++;
+		}
+		
+		return new Goal(scoringTeam,scoringPlayer,time);
+		
+		
+	}
+	
+	public int getHomeGoals() {
+		
+		return this.homeGoals;
+	}
+	public int getAwayGoals() {
+		return this.awayGoals;
 	}
 
 }
--- src/ocasandbox/soccer/League.java
+++ src/ocasandbox/soccer/League.java
@@ -1,10 +1,43 @@
 package ocasandbox.soccer;
 
+import java.util.Random;
+
 public class League {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Player[] homeRoster = new Player[3];
+		Player[] awayRoster = new Player[3];
+		
+		homeRoster[0] = new Player("Bob Smoth");
+		homeRoster[1] = new Player("Sam Houtson");
+		homeRoster[2] = new Player("Jill Jenkins");
+		
+		awayRoster[0] = new Player("Joe Jack");
+		awayRoster[1] = new Player("Sally Salty");
+		awayRoster[2] = new Player("Mike Ike");
+		
+		
+		
+		Team home = new Team("Lions", homeRoster);
+		Team away = new Team("Tigers", awayRoster);
+		
+		Game game = new Game(home, away);
+		
+		int totalGoals = new Random().nextInt(10-3) + 3;
+		Random randomTime = new Random();
+
+		
+		
+		for(int i = 0; i <=totalGoals;i++) {
+			int minutes = randomTime.nextInt(90);
+			double seconds = randomTime.nextDouble();
+			Goal g = game.randomGoal(minutes+seconds);
+			System.out.println(g.getPlayer().getName() + " " + g.getTeam().getTeamName() + " " + g.getTime());
+		}
+		
 
 	}
+	
+	
 
 }
