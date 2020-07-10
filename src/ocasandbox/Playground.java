c0f1e311fe8810243dde8a6b62f59afb1b3c387b
tmoodini <tom.moody@gmail.com>
2020-07-10 09:50:36 -0600

    Commit from Air

--- src/ocasandbox/Playground.java
+++ src/ocasandbox/Playground.java
@@ -22,6 +22,23 @@ public class Playground {
 		pg.printStatInt();
 		pg.setStatInt(5);
 		pg.printStatInt();
+		
+		int x = 5;
+		int y = 6; 
+		int z = x | y;
+		
+		int a = -4;
+		int b = -8;
+		int c = a ^ b;
+				
+		
+		System.out.println(c);
+		int mask = 1;
+		int m = ~6;
+		
+		System.out.println(m);
+		
+		
 	}
 
 }
